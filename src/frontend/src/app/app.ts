794013316d51c52895d15fd4c0a8d4619dc75953
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-10-07 19:09:12 +0200

    pipeline-test

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
