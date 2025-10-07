09ea124095c5ba48b4b628c5e9a2925a473283ee
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-10-07 19:16:23 +0200

    fix-lint

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
