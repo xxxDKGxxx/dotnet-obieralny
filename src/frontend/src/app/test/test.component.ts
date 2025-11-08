258b34d270afa861374840dcf75870ecd14f1e3f
Bartosz Ząbkowski <128218722+bzabk@users.noreply.github.com>
2025-11-08 20:02:23 +0100

    add env files (#30)
    * add env files
    
    * env files fix
    
    * delete prev env folder
    
    * Update src/frontend/src/environments/environment.ts
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
    
    * Update src/frontend/src/environments/environment.development.ts
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
    
    * Update src/frontend/src/environments/environment.production.ts
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
    
    * Update src/frontend/src/environments/environment.ts
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>

--- src/frontend/src/app/test/test.component.ts
+++ src/frontend/src/app/test/test.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
 @Component({
   selector: 'app-test',
   imports: [],
-  template: `<p>test works!</p>`,
+  template: `<p>Test</p>`,
   styleUrl: './test.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
