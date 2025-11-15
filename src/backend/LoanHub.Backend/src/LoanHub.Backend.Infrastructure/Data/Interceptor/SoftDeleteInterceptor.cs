4e302b1ab608a7fc0a5cd2e61a998a9d241193bf
bzabk <bzabkowski@gmail.com>
2025-11-15 17:48:13 +0100

    Add SoftDeleteInterceptor to handle soft deletes in the DbContext

---
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Interceptor/SoftDeleteInterceptor.cs
@@ -0,0 +1,36 @@
+﻿using LoanHub.Aggregator.Core;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace LoanHub.Aggregator.Infrastructure.Data.Interceptors;
+
+public sealed class SoftDeleteInterceptor : SaveChangesInterceptor
+{
+	public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+		DbContextEventData eventData,
+		InterceptionResult<int> result,
+		CancellationToken cancellationToken = new CancellationToken())
+	{
+		var entries = eventData.Context?.ChangeTracker.Entries()
+			.Where(e =>
+			{
+				return e is { State: EntityState.Deleted, Entity: LoanHubEntityBase };
+			})
+			.ToList();
+
+		if (entries is null)
+		{
+			return base.SavingChangesAsync(eventData, result, cancellationToken);
+		}
+
+		foreach (var entityEntry in entries)
+		{
+			entityEntry.State = EntityState.Modified;
+
+			var loanHubEntity = entityEntry.Entity as LoanHubEntityBase;
+
+			loanHubEntity?.Delete();
+		}
+
+		return base.SavingChangesAsync(eventData, result, cancellationToken);
+	}
+}
