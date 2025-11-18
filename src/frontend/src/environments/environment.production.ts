4ea8a2fa25671dbd26a1b77afe2ffe2be6c577d7
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2025-11-18 19:53:31 +0100

    eslint warning fix

--- src/frontend/src/environments/environment.production.ts
+++ src/frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://loanhub-aggregator.internal.bravemoss-1374d09b.polandcentral.azurecontainerapps.io',
+  apiBaseUrl: 'https://loanhub-aggregator.internal.bravemoss-1374d09b.polandcentral.azurecontainerapps.io'
 };
