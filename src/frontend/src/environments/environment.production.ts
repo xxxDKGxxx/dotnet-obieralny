8e36adb1e0e7442dcf8420520f66c05f1d00eb48
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2025-11-18 20:02:27 +0100

    attempt to fix lint warnings

--- src/frontend/src/environments/environment.production.ts
+++ src/frontend/src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://loanhub-aggregator.internal.bravemoss-1374d09b.polandcentral.azurecontainerapps.io'
+  apiBaseUrl:
+    'https://loanhub-aggregator.internal.bravemoss-1374d09b.polandcentral.azurecontainerapps.io',
 };
