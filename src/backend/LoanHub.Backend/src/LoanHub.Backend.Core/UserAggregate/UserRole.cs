b4caae183d5e955bc9ecca702969398ae865cff8
Paula <paula.wolkowska@gmail.com>
2025-11-23 11:14:26 +0100

    add user entity and userrole

---
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/UserAggregate/UserRole.cs
@@ -0,0 +1,28 @@
+using Ardalis.SmartEnum;
+
+namespace LoanHub.Backend.Core.UserAggregate;
+
+public class UserRole(
+    string name,
+    string value) : 
+    SmartEnum<UserRole, string>(name, value)
+{
+    public static readonly UserRole User = new UserRoleType();
+    public static readonly UserRole Employee = new EmployeeRoleType();
+    public static readonly UserRole Admin = new AdminRoleType();
+
+    private sealed class UserRoleType() : 
+        UserRole(nameof(UserRoleType), nameof(User))
+    {
+    }
+
+    private sealed class EmployeeRoleType() : 
+        UserRole(nameof(EmployeeRoleType), nameof(Employee))
+    {
+    }
+
+    private sealed class AdminRoleType() : 
+        UserRole(nameof(AdminRoleType), nameof(Admin))
+    {
+    }
+}
