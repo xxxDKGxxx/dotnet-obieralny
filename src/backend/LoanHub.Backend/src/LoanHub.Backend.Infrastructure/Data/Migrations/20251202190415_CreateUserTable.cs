bf5614bfd903e7ffa1553e1cfe41a456fa0cc9b7
Paula <paula.wolkowska@gmail.com>
2025-12-02 20:06:18 +0100

    fixes

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Migrations/20251202190415_CreateUserTable.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Migrations/20251202190415_CreateUserTable.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace LoanHub.Backend.Infrastructure.Data.Migrations
+namespace LoanHub.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class CreateUserTable : Migration
 {
-    /// <inheritdoc />
-    public partial class CreateUserTable : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
 
-        }
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
 
-        }
-    }
+	}
 }
