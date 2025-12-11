ad080f3dcaf6a72ccdc5006652a9650bb2c4d94b
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-12-11 18:07:08 +0100

    WIP2 waiting for User table

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/OfferAggregate/OfferAggregate.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/OfferAggregate/OfferAggregate.cs
@@ -8,6 +8,9 @@ public sealed class OfferAggregate : LoanHubEntityBase, IAggregateRoot
 	public string Title { get; private set;}
 
 	public string Description { get; private set; }
+	public AmountRange AmountRange { get; private set; }
+	public InterestRateRange InterestRateRange { get; private set; }
+	public ValidRange ValidRange { get; private set; }
 }
 
 public sealed class AmountRange(decimal min, decimal max) : ValueObject
@@ -18,6 +21,31 @@ public sealed class AmountRange(decimal min, decimal max) : ValueObject
 
 	protected override IEnumerable<object> GetEqualityComponents()
 	{
-		throw new NotImplementedException();
+		yield return Min;
+		yield return Max;
+	}
+}
+
+public sealed class InterestRateRange(decimal min, decimal max) : ValueObject
+{
+	public decimal Min { get; private set; } = min;
+	public decimal Max { get; private set; } = max;
+
+	protected override IEnumerable<object> GetEqualityComponents()
+	{
+		yield return Min;
+		yield return Max;
+	}
+}
+
+public sealed class ValidRange(decimal min, decimal max) : ValueObject
+{
+	public decimal Min { get; private set; } = min;
+	public decimal Max { get; private set; } = max;
+
+	protected override IEnumerable<object> GetEqualityComponents()
+	{
+		yield return Min;
+		yield return Max;
 	}
 }
