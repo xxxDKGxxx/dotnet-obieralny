f06ead5ec781d6c97c3b063cfa25f12516782c55
Paula Wołkowska <paula.wolkowska@gmail.com>
2025-12-12 15:29:30 +0100

    Feature/#91 user table (#48)
    * created user table
    
    * add user entity and userrole
    
    * fixed format issues
    
    * format issues fixed
    
    * fixes
    
    * fixes
    
    * formatting issues fixed
    
    * fixed constants and migration
    
    * style issues fix
    
    * formatowanie naprawione
    
    * formatting

---
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Config/DataSchemaConstants.cs
@@ -0,0 +1,12 @@
+namespace LoanHub.Backend.Infrastructure.Data.Config;
+
+public static class DataSchemaConstants
+{
+	public const int EmailMaxLength = 254;
+	public const int FirstNameMaxLength = 50;
+	public const int LastNameMaxLength = 50;
+	public const int AddressMaxLength = 200;
+	public const int PhoneMaxLength = 9;
+	public const int JobMaxLength = 100;
+	public const string MoneyColumnType = "decimal(18,2)";
+}
--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/GlobalUsings.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Reflection;
 global using Ardalis.GuardClauses;
 global using Ardalis.SharedKernel;
 global using Ardalis.Specification.EntityFrameworkCore;
+global using LoanHub.Backend.Core.UserAggregate;
 global using MailKit.Net.Smtp;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
