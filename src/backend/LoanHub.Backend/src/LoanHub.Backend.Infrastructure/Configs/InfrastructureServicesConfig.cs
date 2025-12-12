b394a8bd20a548fb1739d4651be47e25df5f1767
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2025-12-12 14:30:22 +0100

    format fixed

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Configs/InfrastructureServicesConfig.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Configs/InfrastructureServicesConfig.cs
@@ -22,10 +22,10 @@ public static class InfrastructureServicesConfig
 		}
 
 		services.AddApplicationDbContext(connectionString);
-		services.AddAutoMapperConfigs(); ;
+		services.AddAutoMapperConfigs();
+		;
 		services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>))
 			   .AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
-			   
 
 		logger.LogInformation("{Project} services registered", "Infrastructure");
 
