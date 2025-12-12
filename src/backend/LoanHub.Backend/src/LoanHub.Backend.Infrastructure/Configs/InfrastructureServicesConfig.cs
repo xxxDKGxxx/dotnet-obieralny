5958def9e0aef8e602a9b3ce40daa76f4bba01d1
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2025-12-12 14:27:28 +0100

    split adding config into separate operations

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Configs/InfrastructureServicesConfig.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Configs/InfrastructureServicesConfig.cs
@@ -22,9 +22,10 @@ public static class InfrastructureServicesConfig
 		}
 
 		services.AddApplicationDbContext(connectionString);
+		services.AddAutoMapperConfigs(); ;
 		services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>))
-			   .AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>))
-			   .AddAutoMapperConfigs();
+			   .AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+			   
 
 		logger.LogInformation("{Project} services registered", "Infrastructure");
 
