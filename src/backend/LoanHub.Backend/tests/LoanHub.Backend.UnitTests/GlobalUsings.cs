150ca4c4e4ae68af3e47caa12855a093f20ec191
j-was <162911094+j-was@users.noreply.github.com>
2025-12-14 21:51:34 +0100

    Basic unit tests for backend (#52)
    * basic unit tests for backend
    
    * format
    
    * cosmetics
    
    * format fixed - did not fix automatically

--- src/backend/LoanHub.Backend/tests/LoanHub.Backend.UnitTests/GlobalUsings.cs
+++ src/backend/LoanHub.Backend/tests/LoanHub.Backend.UnitTests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using System.Runtime.CompilerServices;
 global using Ardalis.SharedKernel;
+global using LoanHub.Backend.Core;
 global using MediatR;
 global using Microsoft.Extensions.Logging;
 global using NSubstitute;
