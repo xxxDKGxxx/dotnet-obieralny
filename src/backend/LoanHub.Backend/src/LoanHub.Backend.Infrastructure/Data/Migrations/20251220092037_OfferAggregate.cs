100b9fd6b5ef5db42f24d6aa32ffe3d79771a3c4
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-12-20 10:23:14 +0100

    dotnet format

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Migrations/20251220092037_OfferAggregate.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Infrastructure/Data/Migrations/20251220092037_OfferAggregate.cs
@@ -1,65 +1,67 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace LoanHub.Backend.Infrastructure.Data.Migrations
+namespace LoanHub.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class OfferAggregate : Migration
 {
-    /// <inheritdoc />
-    public partial class OfferAggregate : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<bool>(
-                name: "IsDeleted",
-                table: "Users",
-                type: "bit",
-                nullable: false,
-                defaultValue: false,
-                oldClrType: typeof(bool),
-                oldType: "bit");
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AlterColumn<bool>(
+			name: "IsDeleted",
+			table: "Users",
+			type: "bit",
+			nullable: false,
+			defaultValue: false,
+			oldClrType: typeof(bool),
+			oldType: "bit");
 
-            migrationBuilder.CreateTable(
-                name: "Offers",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    AmountRange_Min = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    AmountRange_Max = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    DurationRange_Min = table.Column<long>(type: "bigint", nullable: false),
-                    DurationRange_Max = table.Column<long>(type: "bigint", nullable: false),
-                    InterestRateRange_Min = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    InterestRateRange_Max = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    ValidRange_Min = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    ValidRange_Max = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Offers", x => x.Id);
-                });
-        }
+		migrationBuilder.CreateTable(
+			name: "Offers",
+			columns: table =>
+			{
+				return new
+				{
+					Id = table.Column<int>(type: "int", nullable: false)
+									.Annotation("SqlServer:Identity", "1, 1"),
+					Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+					Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					AmountRange_Min = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+					AmountRange_Max = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+					DurationRange_Min = table.Column<long>(type: "bigint", nullable: false),
+					DurationRange_Max = table.Column<long>(type: "bigint", nullable: false),
+					InterestRateRange_Min = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+					InterestRateRange_Max = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+					ValidRange_Min = table.Column<DateTime>(type: "datetime2", nullable: false),
+					ValidRange_Max = table.Column<DateTime>(type: "datetime2", nullable: false),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+					IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+				};
+			},
+			constraints: table =>
+			{
+				table.PrimaryKey("PK_Offers", x => x.Id);
+			});
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Offers");
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropTable(
+			name: "Offers");
 
-            migrationBuilder.AlterColumn<bool>(
-                name: "IsDeleted",
-                table: "Users",
-                type: "bit",
-                nullable: false,
-                oldClrType: typeof(bool),
-                oldType: "bit",
-                oldDefaultValue: false);
-        }
-    }
+		migrationBuilder.AlterColumn<bool>(
+			name: "IsDeleted",
+			table: "Users",
+			type: "bit",
+			nullable: false,
+			oldClrType: typeof(bool),
+			oldType: "bit",
+			oldDefaultValue: false);
+	}
 }
