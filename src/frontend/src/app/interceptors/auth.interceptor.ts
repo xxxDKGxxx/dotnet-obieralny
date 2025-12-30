e6d44c78be9d3f17c8d14338cc5eb0ef152d0bce
Paula <paula.wolkowska@gmail.com>
2025-12-30 11:28:59 +0100

    fixes

--- src/frontend/src/app/interceptors/auth.interceptor.ts
+++ src/frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,15 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('auth_token');
+  const platformId = inject(PLATFORM_ID);
 
-  if (token) {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next(clonedRequest);
+  if (isPlatformBrowser(platformId)) {
+    const token = globalThis.localStorage?.getItem('auth_token');
+
+    if (token) {
+      const clonedRequest = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return next(clonedRequest);
+    }
   }
 
   return next(req);
