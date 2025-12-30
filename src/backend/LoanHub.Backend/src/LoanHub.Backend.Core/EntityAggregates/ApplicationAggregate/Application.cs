76e62c8ec5b49ec74241c96c6386fadb9c9d4ed2
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2025-12-30 14:11:17 +0100

    application class in backend

---
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/Application.cs
@@ -0,0 +1,42 @@
+using LoanHub.Backend.Core.EntityAggregates.OfferAggregate;
+
+namespace LoanHub.Backend.Core.EntityAggregates.ApplicationAggregate;
+
+public sealed class Application :
+	LoanHubEntityBase,
+	IAggregateRoot
+{
+	public string Title { get; private set; }
+	public string Description { get; private set; }
+	public int OfferId { get; private set; }
+	public int UserId { get; private set; }
+	public int? BankEmployeeId { get; private set; }
+	public ApplicationStatus Status { get; private set; }
+	public decimal Amount { get; private set; }
+	public uint Duration { get; private set; }
+	public decimal InterestRate { get; private set; }
+	public DateTime UpdatedAt { get; private set; }
+
+	public Application(string title, string description, int offerId, int userId, decimal amount, uint duration, decimal interestRate, ApplicationStatus? status = null)
+	{
+		Title = title;
+		Description = description;
+		OfferId = offerId;
+		UserId = userId;
+		Status = status ?? ApplicationStatus.Created;
+		Amount = amount;
+		Duration = duration;
+		InterestRate = interestRate;
+		UpdatedAt = DateTime.UtcNow;
+	}
+
+	private void SetStatus(ApplicationStatus status)
+	{
+		Status = status;
+	}
+
+	private void SetBankEmployee(int? bankEmployeeId)
+	{
+		BankEmployeeId = bankEmployeeId;
+	}
+}
---
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/ApplicationStatus.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanHub.Backend.Core.EntityAggregates.ApplicationAggregate;
+
+public class ApplicationStatus(
+	string name,
+	string value) :
+	SmartEnum<ApplicationStatus, string>(name, value)
+{
+	public static readonly ApplicationStatus Created = new CreatedApplicationStatus();
+	public static readonly ApplicationStatus Submitted = new SubmittedApplicationStatus();
+	public static readonly ApplicationStatus Reviewed = new ReviewedApplicationStatus();
+	public static readonly ApplicationStatus AwaitingSignature = new AwaitingSignatureApplicationStatus();
+	public static readonly ApplicationStatus Signed = new SignedApplicationStatus();
+	public static readonly ApplicationStatus Granted = new GrantedApplicationStatus();
+	public static readonly ApplicationStatus Rejected = new RejectedApplicationStatus();
+	public static readonly ApplicationStatus Withdrawn = new WithdrawnApplicationStatus();
+
+	private sealed class CreatedApplicationStatus() :
+		ApplicationStatus(nameof(CreatedApplicationStatus), nameof(Created))
+	{
+	}
+	private sealed class SubmittedApplicationStatus() :
+		ApplicationStatus(nameof(SubmittedApplicationStatus), nameof(Submitted))
+	{
+	}
+
+	private sealed class ReviewedApplicationStatus() :
+		ApplicationStatus(nameof(ReviewedApplicationStatus), nameof(Reviewed))
+	{
+	}
+
+	private sealed class AwaitingSignatureApplicationStatus() :
+		ApplicationStatus(nameof(AwaitingSignatureApplicationStatus), nameof(AwaitingSignature))
+	{
+	}
+
+	private sealed class SignedApplicationStatus() :
+		ApplicationStatus(nameof(SignedApplicationStatus), nameof(Signed))
+	{
+	}
+
+	private sealed class GrantedApplicationStatus() :
+		ApplicationStatus(nameof(GrantedApplicationStatus), nameof(Granted))
+	{
+	}
+
+	private sealed class RejectedApplicationStatus() :
+		ApplicationStatus(nameof(RejectedApplicationStatus), nameof(Rejected))
+	{
+	}
+
+	private sealed class WithdrawnApplicationStatus() :
+		ApplicationStatus(nameof(WithdrawnApplicationStatus), nameof(Withdrawn))
+	{
+	}
+}
