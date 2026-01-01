7dacacdcb0167804bcebeb443ecc9a109bdb66aa
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-01-01 16:28:43 +0100

    Namespace fixes

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.UseCases/GlobalUsings.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.UseCases/GlobalUsings.cs
@@ -2,7 +2,7 @@ global using System.Reflection;
 global using Ardalis.Result;
 global using Ardalis.SharedKernel;
 global using LoanHub.Backend.Core.Interfaces;
-global using LoanHub.Backend.Core.UserAggregate;
+global using LoanHub.Backend.Core.EntityAggregates.UserAggregate;
 global using MediatR;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
