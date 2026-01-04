9b299bbae5983a17a90283a1c90b54baf2dc6e08
Bartosz Ząbkowski <128218722+bzabk@users.noreply.github.com>
2026-01-04 15:51:31 +0100

    Apply suggestions from code review
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>

--- src/frontend/src/app/services/counter.service.ts
+++ src/frontend/src/app/services/counter.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-export interface CounterData {
+export interface CounterResponse {
   count: number;
 }
 
