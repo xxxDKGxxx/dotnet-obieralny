0f0b83c5fd9122f86fb23fe8c0b5f26ff312f4dc
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-01-12 20:59:26 +0100

    WIP calculated endpoint

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Web/Endpoints/Offers/Get/GetCalculated.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Web/Endpoints/Offers/Get/GetCalculated.cs
@@ -1,6 +1,11 @@
 namespace LoanHub.Backend.Web.Endpoints.Offers.Get;
 
-public class GetCalculated : Endpoint<>
+public class GetCalculated : Endpoint<GetCalculatedOfferRequest, Result<CalculatedOfferDto>>
 {
-
+	public override void Configure()
+	{
+		AllowAnonymous();
+		Version(1);
+		Get("/calculated-offers/{OfferId:int}");
+	}
 }
