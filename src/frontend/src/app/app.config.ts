0c0cd23049c5075b0907dabf8948465ecfdf8269
bzabk <bzabkowski@gmail.com>
2026-01-15 12:03:19 +0100

    remove duplicated import

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -8,7 +8,6 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { errorInterceptor } from './interceptors/error.interceptor';
 import { routes } from './app.routes';
-import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
