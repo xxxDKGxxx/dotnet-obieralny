43ac31fb3bef3c7092a5a1a674df718ea4c9688b
Bartosz Ząbkowski <128218722+bzabk@users.noreply.github.com>
2026-01-16 23:19:08 +0100

    front part of counter implementation (#54)
    * front part of counter implementation
    
    * eslint fix
    
    * eslint fix
    
    * adding backend part
    
    * dotnet format
    
    * format fix
    
    * dotnet format fix
    
    * only frontend part
    
    * prettier
    
    * format dotnet
    
    * delete files
    
    * restore changes
    
    * width change
    
    * error fix
    
    * increase budget size
    
    * Apply suggestions from code review
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
    
    * fixes
    
    * fixes
    
    * remove duplicated import
    
    * increase budget
    
    * fixes
    
    * text change
    
    ---------
    
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -6,7 +6,6 @@ import { HeaderBar } from './header-bar/header-bar';
   selector: 'app-root',
   imports: [RouterOutlet, HeaderBar],
   templateUrl: './app.html',
-  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
