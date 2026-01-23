b57ed0969bdb263e8fac15b0a2dfda11423e3dd4
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2026-01-23 20:36:08 +0100

    application status refactored

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/Application.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/Application.cs
@@ -27,7 +27,7 @@ public sealed class Application :
 		ApplicantFinancials = financials;
 		PersonalData = personalData;
 		OfferConditions = conditions;
-		Status = ApplicationStatus.Created;
+		Status = ApplicationStatus.Submitted;
 	}
 
 #pragma warning disable CS8618
