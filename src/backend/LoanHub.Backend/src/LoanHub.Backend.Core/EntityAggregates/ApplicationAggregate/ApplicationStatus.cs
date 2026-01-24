d36ad5d8f259e6f526731c558e9be2761291ed1a
j-was <162911094+j-was@users.noreply.github.com>
2026-01-24 14:15:40 +0100

    Application status refactor (#82)
    * application status refactored
    
    * submitted renamed to created

--- src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/ApplicationStatus.cs
+++ src/backend/LoanHub.Backend/src/LoanHub.Backend.Core/EntityAggregates/ApplicationAggregate/ApplicationStatus.cs
@@ -5,26 +5,43 @@ public class ApplicationStatus(
 	string value) :
 	SmartEnum<ApplicationStatus, string>(name, value)
 {
+	/// <summary>
+	/// Client has applied for a loan
+	/// </summary>
 	public static readonly ApplicationStatus Created = new CreatedApplicationStatus();
-	public static readonly ApplicationStatus Submitted = new SubmittedApplicationStatus();
-	public static readonly ApplicationStatus Reviewed = new ReviewedApplicationStatus();
+
+	/// <summary>
+	/// Bank employee has reviewed and accepted the application, email with document sent to client
+	/// </summary>
 	public static readonly ApplicationStatus AwaitingSignature = new AwaitingSignatureApplicationStatus();
+
+	/// <summary>
+	/// Client has sent back the signed document
+	/// </summary>
 	public static readonly ApplicationStatus Signed = new SignedApplicationStatus();
+
+	/// <summary>
+	/// Loan has been granted after signed document verification
+	/// </summary>
 	public static readonly ApplicationStatus Granted = new GrantedApplicationStatus();
+
+	/// <summary>
+	/// Client has to re-send a signed document after amendments
+	/// </summary>
+	public static readonly ApplicationStatus AwaitingAmendments = new AwaitingAmendmentsApplicationStatus();
+
+	/// <summary>
+	/// Application has been rejected by the bank
+	/// </summary>
 	public static readonly ApplicationStatus Rejected = new RejectedApplicationStatus();
+
+	/// <summary>
+	/// Client has withdrawn the application
+	/// </summary>
 	public static readonly ApplicationStatus Withdrawn = new WithdrawnApplicationStatus();
 
 	private sealed class CreatedApplicationStatus() :
 		ApplicationStatus(nameof(CreatedApplicationStatus), nameof(Created))
-	{
-	}
-	private sealed class SubmittedApplicationStatus() :
-		ApplicationStatus(nameof(SubmittedApplicationStatus), nameof(Submitted))
-	{
-	}
-
-	private sealed class ReviewedApplicationStatus() :
-		ApplicationStatus(nameof(ReviewedApplicationStatus), nameof(Reviewed))
 	{
 	}
 
@@ -40,6 +57,11 @@ public class ApplicationStatus(
 
 	private sealed class GrantedApplicationStatus() :
 		ApplicationStatus(nameof(GrantedApplicationStatus), nameof(Granted))
+	{
+	}
+
+	private sealed class AwaitingAmendmentsApplicationStatus() :
+	ApplicationStatus(nameof(AwaitingAmendmentsApplicationStatus), nameof(AwaitingAmendments))
 	{
 	}
 
