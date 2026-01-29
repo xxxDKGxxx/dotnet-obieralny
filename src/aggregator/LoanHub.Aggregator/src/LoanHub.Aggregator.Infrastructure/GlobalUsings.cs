45a2d79a9f5d6c768da74332cee52f37c19e8252
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-01-29 15:33:52 +0100

    Global using move

--- src/aggregator/LoanHub.Aggregator/src/LoanHub.Aggregator.Infrastructure/GlobalUsings.cs
+++ src/aggregator/LoanHub.Aggregator/src/LoanHub.Aggregator.Infrastructure/GlobalUsings.cs
@@ -1,3 +1,4 @@
+global using System.Net.Http.Headers;
 global using System.Net.Http.Json;
 global using System.Reflection;
 global using System.Text.Json;
--- src/aggregator/LoanHub.Aggregator/src/LoanHub.Aggregator.Infrastructure/OfferProviders/ArdalisBankOfferProvider.cs
+++ src/aggregator/LoanHub.Aggregator/src/LoanHub.Aggregator.Infrastructure/OfferProviders/ArdalisBankOfferProvider.cs
@@ -1,5 +1,3 @@
-using System.Net.Http.Headers;
-
 namespace LoanHub.Aggregator.Infrastructure.OfferProviders;
 
 public sealed record OfferDto(
